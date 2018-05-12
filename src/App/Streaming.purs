031079ec16b91f7dae8968e3c1facbdf12f13121
Michael Dougherty <maackle.d@gmail.com>
2018-05-12 11:45:57 -0700

    PlayMove done but without validation

--- src/App/Streaming.purs
+++ src/App/Streaming.purs
@@ -3,16 +3,19 @@ module App.Streaming where
 import Prelude
 
 import App.Common (messageTypeString)
-import App.IgoMsg (AcceptMatchPayload, DeclineMatchPayload, IgoMsg(..), MsgKey, OfferMatchPayload, RequestMatchPayload, SsbMessage(SsbMessage), parseMessage)
+import App.IgoMsg (AcceptMatchPayload, BoardPosition(..), DeclineMatchPayload, IgoMsg(..), MsgKey, OfferMatchPayload, RequestMatchPayload, SsbMessage(SsbMessage), PlayMovePayload, parseMessage)
 import App.Utils (trace')
 import Data.Argonaut (Json, jsonNull, toObject, toString)
+import Data.Array (snoc)
 import Data.Either (Either(..))
 import Data.Function.Uncurried (Fn2)
 import Data.Generic (class Generic, gEq, gShow)
 import Data.Maybe (Maybe(..), maybe)
 import Data.StrMap (StrMap, delete, insert, lookup)
 import Data.StrMap as M
-import Partial.Unsafe (unsafeCrashWith)
+import Data.Tuple.Nested ((/\))
+import Partial (crashWith)
+import Partial.Unsafe (unsafeCrashWith, unsafePartial)
 import Ssb.Types (UserKey)
 
 
@@ -20,7 +23,8 @@ type FlumeDb =
   { offers :: StrMap IndexedOffer
   , declines :: StrMap IndexedDecline
   , requests :: StrMap IndexedRequest
-  , matches :: StrMap IndexedAccept
+  , matches :: StrMap IndexedMatch
+  , moves :: StrMap IndexedMove
   }
 
 initialDb :: FlumeDb
@@ -29,28 +33,35 @@ initialDb =
   , declines: M.empty
   , requests: M.empty
   , matches: M.empty
+  , moves: M.empty
   }
 
 -- TODO: make newtype
 data IndexedOffer = IndexedOffer OfferMatchPayload {author :: UserKey}
 data IndexedDecline = IndexedDecline DeclineMatchPayload {author :: UserKey}
 data IndexedRequest = IndexedRequest RequestMatchPayload {author :: UserKey}
-data IndexedAccept = IndexedAccept AcceptMatchPayload {author :: UserKey}
+data IndexedMatch = IndexedMatch AcceptMatchPayload (Array MoveStep) {author :: UserKey}
+data IndexedMove = IndexedMove PlayMovePayload {rootAccept :: MsgKey} {author :: UserKey}
+newtype MoveStep = MoveStep {position :: BoardPosition, key :: MsgKey}
 
 derive instance genericIndexedOffer :: Generic IndexedOffer
 derive instance genericIndexedDecline :: Generic IndexedDecline
 derive instance genericIndexedRequest :: Generic IndexedRequest
-derive instance genericIndexedAccept :: Generic IndexedAccept
+derive instance genericIndexedMatch :: Generic IndexedMatch
+derive instance genericIndexedMove :: Generic IndexedMove
+derive instance genericMoveStep :: Generic MoveStep
 
 instance showIndexedOffer :: Show IndexedOffer where show = gShow
 instance showIndexedDecline :: Show IndexedDecline where show = gShow
 instance showIndexedRequest :: Show IndexedRequest where show = gShow
-instance showIndexedAccept :: Show IndexedAccept where show = gShow
+instance showIndexedMatch :: Show IndexedMatch where show = gShow
+instance showIndexedMove :: Show IndexedMove where show = gShow
 
 instance eqIndexedOffer :: Eq IndexedOffer where eq = gEq
 instance eqIndexedDecline :: Eq IndexedDecline where eq = gEq
 instance eqIndexedRequest :: Eq IndexedRequest where eq = gEq
-instance eqIndexedAccept :: Eq IndexedAccept where eq = gEq
+instance eqIndexedMatch :: Eq IndexedMatch where eq = gEq
+instance eqIndexedMove :: Eq IndexedMove where eq = gEq
 
 type ReduceFn = FlumeDb -> Json -> FlumeDb
 type ReduceFnImpl = Fn2 FlumeDb Json FlumeDb
@@ -69,9 +80,8 @@ reduceFn db json =
       db { requests = insert key (IndexedRequest payload {author}) db.requests }
 
     SsbMessage (ExpireRequest targetKey) {author} ->
-      case lookup targetKey db.requests of
-        Nothing -> db
-        Just (IndexedRequest _ meta) ->
+      lookup targetKey db.requests
+        # maybe db \(IndexedRequest _ meta) ->
           if author == meta.author
             then db { requests = delete targetKey db.requests }
             else db
@@ -79,10 +89,25 @@ reduceFn db json =
     SsbMessage (OfferMatch payload) {key, author} ->
       db { offers = insert key (IndexedOffer payload {author}) db.offers }
 
+    SsbMessage (WithdrawOffer targetKey) {author} ->
+      lookup targetKey db.offers
+        # maybe db \(IndexedOffer _ meta) ->
+          if author == meta.author
+            then db { offers = delete targetKey db.offers }
+            else db
+
+    SsbMessage (AcceptMatch payload@{offerKey}) {key, author} ->
+      lookup offerKey db.offers
+        # maybe db \(IndexedOffer {opponentKey} meta) ->
+          if author == opponentKey
+            then db { offers = delete offerKey db.offers
+                    , matches = insert key (IndexedMatch payload [] {author}) db.matches
+                    }
+            else db
+
     SsbMessage (DeclineMatch payload@{offerKey}) {key, author} ->
-      case lookup offerKey db.offers of
-        Nothing -> db
-        Just (IndexedOffer {opponentKey} meta) ->
+      lookup offerKey db.offers
+        # maybe db \(IndexedOffer {opponentKey} meta) ->
           if author == opponentKey
             then db { offers = delete offerKey db.offers
                     , declines = insert key (IndexedDecline payload {author}) db.declines
@@ -90,14 +115,36 @@ reduceFn db json =
             else db
 
     SsbMessage (AcknowledgeDecline targetKey) {author} ->
-      case lookup targetKey db.declines of
-        Nothing -> db
-        Just (IndexedDecline {userKey} meta) ->
+      lookup targetKey db.declines
+        # maybe db \(IndexedDecline {userKey} meta) ->
           if author == userKey
             then db { declines = delete targetKey db.declines }
             else db
 
-    _ -> trace' "TODO :()" db
+    SsbMessage (PlayMove payload@{position, lastMove}) {author, key} ->
+      let
+        triple = do
+          -- if no moves so far, then lastMove refers to the root Accept message
+          -- else, get the rootAccept key from db.moves
+          let rootAccept = M.lookup lastMove db.moves
+                         # maybe lastMove \(IndexedMove _ {rootAccept} _) -> rootAccept
+          IndexedMatch matchPayload matchMoves matchMeta <- M.lookup rootAccept db.matches
+          let
+            newMoves = snoc matchMoves $ MoveStep {position, key}
+            newMatch = IndexedMatch matchPayload newMoves matchMeta
+          pure
+            $ rootAccept /\ IndexedMove payload {rootAccept} {author} /\ newMatch
+
+      in case triple of
+        Just (rootAccept /\ move /\ match@(IndexedMatch _ _ meta)) ->
+          db { moves = M.insert key move db.moves
+             , matches = M.insert rootAccept match db.matches }
+        Nothing -> trace' ("move not found: " <> lastMove) db
+
+    SsbMessage (Kibitz payload) _ ->
+      trace' "TODO" db
+
+
   where
     msg = case parseMessage json of
       Right m -> m
--- test/Main.purs
+++ test/Main.purs
@@ -3,8 +3,8 @@ module Test.Main where
 import Prelude
 
 import App.DB.Main (ssbIgoPlugin)
-import App.IgoMsg (IgoMsg(..), OfferMatchPayload, RequestMatchPayload, SsbMessage(..), StoneColor(..), publishMsg')
-import App.Streaming (FlumeDb, IndexedDecline(..), IndexedOffer(..), IndexedRequest(..))
+import App.IgoMsg (BoardPosition(..), GameTerms, IgoMsg(..), MsgKey, OfferMatchPayload, RequestMatchPayload, SsbMessage(..), StoneColor(..), publishMsg')
+import App.Streaming (FlumeDb, IndexedDecline(..), IndexedMatch(..), IndexedOffer(..), IndexedRequest(..), MoveStep(..))
 import App.UI.ClientQueries (getDb)
 import App.Utils ((!))
 import Control.Monad.Aff (Aff, attempt)
@@ -18,6 +18,7 @@ import Data.Array.NonEmpty (NonEmptyArray, replicate)
 import Data.Bifunctor (lmap)
 import Data.Either (Either(..), either, fromRight)
 import Data.Foreign (toForeign)
+import Data.Int (toNumber)
 import Data.Maybe (Maybe(..))
 import Data.StrMap as M
 import Data.Traversable (sequence, traverse)
@@ -66,8 +67,8 @@ playbook3 sbot play = do
   c <- feed sbot
   play a b c
 
-withSession :: (Aff FX Conn) -> (Conn -> Aff FX Unit) -> Aff FX Unit
-withSession create runTest = do
+sesh :: (Aff FX Conn) -> (Conn -> Aff FX Unit) -> Aff FX Unit
+sesh create runTest = do
   sbot <- create
   result <- attempt $ runTest sbot
   close sbot
@@ -113,46 +114,66 @@ main = do
       { terms: testTerms
       }
 
-    offerPayload :: UserKey -> OfferMatchPayload
-    offerPayload opponentKey =
+    offerPayload :: Conn -> OfferMatchPayload
+    offerPayload = offerPayload' <<< _.id <<< props
+
+    offerPayload' :: UserKey -> OfferMatchPayload
+    offerPayload' opponentKey =
       { myColor: Black
       , opponentKey
       , terms: testTerms
       }
 
+    setupMatch :: Conn -> Conn -> StoneColor -> Int -> Aff FX MsgKey
+    setupMatch alice bob myColor handicap = do
+      let
+        terms = {size: 19, komi: 0.5, handicap}
+        opponentKey = props bob # _.id
+        offerData = {myColor, opponentKey, terms}
+      (SsbMessage _ offerMeta) <- publishMsg' alice $ OfferMatch offerData
+      (SsbMessage _ acceptMeta) <- publishMsg' bob $ AcceptMatch {offerKey: offerMeta.key, terms}
+      pure acceptMeta.key
+
+    playMove :: Conn -> MsgKey -> Aff FX MsgKey
+    playMove agent lastMove = do
+      let
+        position = BoardPosition 0 0
+        payload = {position, lastMove, subjectiveMoveNum: -1}
+      (SsbMessage _ {key}) <- publishMsg' agent $ PlayMove payload
+      pure key
 
   run [consoleReporter] do
     describe "server" do
-      it "can connect" $ withSession testbot \sbot -> do
+      it "can connect" $ sesh testbot \sbot -> do
         {id} <- whoami sbot
         let s = props sbot
         shouldEqual id s.id
-      it "has feeds" $ withSession testbot \sbot -> do
+      it "has feeds" $ sesh testbot \sbot -> do
         alice <- feed sbot
         bob <- feed sbot
         shouldNotEqual (alice # props # _.id) (bob # props # _.id)
 
     describe "client" do
-      pending' "can connect" $ withSession fullbot \sbot -> do
+      pending' "can connect" $ sesh fullbot \sbot -> do
         client <- getClient =<< liftEff normalConfig
         traceA "zee client"
         traceAnyA client
         shouldEqual 1 1
 
     describe "ssb-igo" do
-      it "has a db" $ withSession testbot \sbot -> do
+      it "has a db" $ sesh testbot \sbot -> do
         db <- getDb sbot
         db.offers `shouldEqual` M.empty
 
       describe "RequestMatch" do
-        it "indexes requests" $ withSession testbot \sbot -> do
+        it "indexes requests" $ sesh testbot \sbot -> do
           playbook1 sbot \alice -> do
             (SsbMessage _ {author, key}) <- publishMsg' alice $ RequestMatch requestPayload
             db <- getDb sbot
             M.lookup key db.requests `shouldEqual` (Just $ IndexedRequest requestPayload {author})
 
       describe "ExpireRequest" do
-        it "only cancels requests from original author" $ withSession testbot \sbot -> do
+        it "only cancels requests from original author" $ sesh testbot \sbot -> do
           playbook2 sbot \alice bob -> do
             (SsbMessage _ {author, key}) <- publishMsg' alice $ RequestMatch requestPayload
             let expected = IndexedRequest requestPayload {author}
@@ -168,17 +189,17 @@ main = do
             db.requests `shouldEqual` M.empty
 
       describe "OfferMatch" do
-        it "indexes offers" $ withSession testbot \sbot -> do
+        it "indexes offers" $ sesh testbot \sbot -> do
           playbook2 sbot \alice bob -> do
-            let payload = offerPayload $ props bob # _.id
+            let payload = offerPayload bob
             (SsbMessage _ {author, key}) <- publishMsg' alice $ OfferMatch payload
             checkDb sbot \db ->
               M.lookup key db.offers `shouldEqual` (Just $ IndexedOffer payload {author})
 
       describe "DeclineMatch" do
-        it "lets target of OfferMatch decline" $ withSession testbot \sbot -> do
+        it "lets target of OfferMatch decline" $ sesh testbot \sbot -> do
           playbook2 sbot \alice bob -> do
-            let offerData = offerPayload $ props bob # _.id
+            let offerData = offerPayload bob
             (SsbMessage _ {author, key}) <- publishMsg' alice $ OfferMatch offerData
             checkDb sbot \db ->
               M.lookup key db.offers `shouldEqual` (Just $ IndexedOffer offerData {author})
@@ -186,5 +207,104 @@ main = do
             let declinePayload = {offerKey: key, userKey: author, reason: Just "changed my mind"}
             (SsbMessage _ declineMeta) <- publishMsg' bob $ DeclineMatch declinePayload
             checkDb sbot \db -> do
-              M.lookup declineMeta.key db.offers `shouldEqual` Nothing
+              db.offers `shouldEqual` M.empty
               M.values db.declines `shouldEqual` [IndexedDecline declinePayload {author: declineMeta.author}]
+
+        it "prevents all others from declining" $ sesh testbot \sbot -> do
+          playbook3 sbot \alice bob charlie -> do
+            let offerData = offerPayload bob
+            (SsbMessage _ {author, key}) <- publishMsg' alice $ OfferMatch offerData
+            let expected = IndexedOffer offerData {author}
+            let declinePayload = {offerKey: key, userKey: author, reason: Just "changed my mind"}
+
+            (SsbMessage _ declineMeta) <- publishMsg' alice $ DeclineMatch declinePayload
+            checkDb sbot \db -> do
+              M.values db.offers `shouldEqual` [expected]
+              db.declines `shouldEqual` M.empty
+
+            (SsbMessage _ declineMeta) <- publishMsg' charlie $ DeclineMatch declinePayload
+            checkDb sbot \db -> do
+              M.values db.offers `shouldEqual` [expected]
+              db.declines `shouldEqual` M.empty
+
+            (SsbMessage _ declineMeta) <- publishMsg' bob $ DeclineMatch declinePayload
+            checkDb sbot \db -> do
+              db.offers `shouldEqual` M.empty
+              M.values db.declines `shouldEqual` [IndexedDecline declinePayload {author: declineMeta.author}]
+
+      describe "AcknowledgeDecline" do
+        it "clears declines from index" $ sesh testbot \sbot -> do
+          playbook3 sbot \alice bob charlie -> do
+            let offerData = offerPayload bob
+            (SsbMessage _ offerMeta) <- publishMsg' alice $ OfferMatch offerData
+            let declinePayload =
+                  { offerKey: offerMeta.key
+                  , userKey: offerMeta.author
+                  , reason: Just "changed my mind" }
+
+            (SsbMessage _ declineMeta) <- publishMsg' bob $ DeclineMatch declinePayload
+            let expected = IndexedDecline declinePayload {author: declineMeta.author}
+            (SsbMessage _ _) <- publishMsg' charlie $ AcknowledgeDecline declineMeta.key
+            (SsbMessage _ _) <- publishMsg' bob $ AcknowledgeDecline declineMeta.key
+            checkDb sbot \db -> do
+              M.values db.declines `shouldEqual` [expected]
+            (SsbMessage _ _) <- publishMsg' alice $ AcknowledgeDecline declineMeta.key
+            checkDb sbot \db -> do
+              db.declines `shouldEqual` M.empty
+
+      describe "WithdrawOffer" do
+        it "lets an offer maker withdraw their own offer" $ sesh testbot \sbot -> do
+          playbook2 sbot \alice bob -> do
+            let offerData = offerPayload bob
+            (SsbMessage _ {key}) <- publishMsg' alice $ OfferMatch offerData
+            (SsbMessage _ _) <- publishMsg' bob $ WithdrawOffer key
+            checkDb sbot \db -> M.size db.offers `shouldEqual` 1
+            (SsbMessage _ _) <- publishMsg' alice $ WithdrawOffer key
+            checkDb sbot \db -> M.size db.offers `shouldEqual` 0
+
+      describe "AcceptMatch" do
+        it "lets only intended opponent accept" $ sesh testbot \sbot -> do
+          playbook3 sbot \alice bob charlie -> do
+            let offerData@{terms} = offerPayload bob
+            (SsbMessage _ {key}) <- publishMsg' alice $ OfferMatch offerData
+            _ <- publishMsg' charlie $ AcceptMatch {offerKey: key, terms}
+            _ <- publishMsg' alice $ AcceptMatch {offerKey: key, terms}
+            checkDb sbot \db -> M.size db.matches `shouldEqual` 0
+            _ <- publishMsg' bob $ AcceptMatch {offerKey: key, terms}
+            checkDb sbot \db -> do
+              M.size db.matches `shouldEqual` 1
+
+      describe "PlayMove" do
+        it "indexes moves" $ sesh testbot \sbot -> do
+          playbook2 sbot \alice bob -> do
+            match <- setupMatch alice bob Black 0
+                  >>= playMove alice >>= playMove bob >>= playMove alice
+            checkDb sbot \db -> do
+              traceAnyA db
+              M.size db.moves `shouldEqual` 3
+
+        it "updates match index" $ sesh testbot \sbot -> do
+          playbook2 sbot \alice bob -> do
+            match <- setupMatch alice bob Black 0
+            m1 <- playMove alice match
+            m2 <- playMove bob m1
+            m3 <- playMove bob m2
+            checkDb sbot \db -> do
+              let
+                moveKeys :: Maybe (Array MsgKey)
+                moveKeys = M.lookup match db.matches <#> \(IndexedMatch _ moves _) ->
+                            moves <#> \(MoveStep {key}) -> key
+              moveKeys `shouldEqual` Just [m1, m2, m3]
+
+        describe "first move detection" do
+          it "does even games" $ sesh testbot \sbot ->
+            playbook2 sbot \alice bob -> do
+              match <- setupMatch alice bob Black 0
+              _ <- playMove bob match
+              checkDb sbot \db -> M.size db.moves `shouldEqual` 0
+              _ <- playMove alice match
+              checkDb sbot \db -> M.size db.moves `shouldEqual` 1
+
+            -- let offerData@{terms} = offerPayload bob
+            -- (SsbMessage _ {key}) <- publishMsg' alice $ OfferMatch offerData
+            -- _ <- publishMsg' bob $ AcceptMatch {offerKey: key, terms}
